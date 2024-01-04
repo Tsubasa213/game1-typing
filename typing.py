501c3764d4d687bdabeff9d06e602ca0362a5997
Sharla <116696387+Tsubasa213@users.noreply.github.com>
2024-01-04 14:21:10 +0900

    update typing.py

--- typing.py
+++ typing.py
@@ -3,5 +3,9 @@ import pygame
 easy_words_a = ["anpanman","ringo","neko","hituji","karaoke","tora","saihu","miso","banana","budou","okaki","mikan"]
 easy_words_b = ["アンパンマン","りんご","ねこ","ひつじ","カラオケ","とら","さいふ","みそ","バナナ","ぶどう","おかき","みかん"]
 
-normal_words_a =
-normal_words_b = 
+normal_words_a = ["ryokousaki"]
+normal_words_b = ["旅行先"]
+
+hard_word_a = ["dhizuni-rando"]
+hard_word_b = ["ディズニーランド"]
+
