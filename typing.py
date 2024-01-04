328cef5368c4dce569f6ee026168225cfe373a68
Sharla <116696387+Tsubasa213@users.noreply.github.com>
2024-01-04 14:04:20 +0900

    update typing.py

---
+++ typing.py
@@ -0,0 +1,7 @@
+import pygame
+
+easy_words_a = ["anpanman","ringo","neko","hituji","karaoke","tora","saihu","miso","banana","budou","okaki","mikan"]
+easy_words_b = ["アンパンマン","りんご","ねこ","ひつじ","カラオケ","とら","さいふ","みそ","バナナ","ぶどう","おかき","みかん"]
+
+normal_words_a =
+normal_words_b = 
